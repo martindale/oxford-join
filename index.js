b9b0655b580f8d47a6bf2ef6edf5d4912f27d4e9
Eric Martindale <eric@ericmartindale.com>
2016-01-04 13:00:19 -0800

    Handle more test cases.

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function oxfordJoin(array) {
   }
 
   var string = '';
-  if (array.length <= 1) {
-    string = array.join();
-  } else {
+  if (array.length >= 3) {
     string = array.slice(0, -1).join(', ') + ', and ' + array[array.length-1];
+  } else if (array.length == 2) {
+    string = array.join(' and ');
+  } else {
+    string = array.join();
   }
   return string;
 }
--- test/oxford-join.unit.js
+++ test/oxford-join.unit.js
@@ -3,9 +3,21 @@ var assert = require('assert');
 
 describe('oxfordJoin', function() {
   describe('#join()', function () {
-    it('should return an appropriate test string.', function () {
+    it('should return handle three items correctly.', function () {
       var invitations = ['the strippers', 'JFK', 'Stalin'];
       assert.equal('the strippers, JFK, and Stalin', oxfordJoin(invitations));
+    });
+    it('should return handle two items correctly.', function () {
+      var invitations = ['the strippers', 'JFK'];
+      assert.equal('the strippers and JFK', oxfordJoin(invitations));
+    });
+    it('should return handle one item correctly.', function () {
+      var invitations = ['the strippers'];
+      assert.equal('the strippers', oxfordJoin(invitations));
+    });
+    it('should return handle zero items correctly.', function () {
+      var invitations = [];
+      assert.equal('', oxfordJoin(invitations));
     });
   });
 });
